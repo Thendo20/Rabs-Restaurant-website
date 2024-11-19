a76b1c6c70ca4e69a4ad80dd100fa6a545196eed
Thendo <thendorabali43@gmail.com>
2024-11-19 20:09:25 +0200

    Backend and frontend

---
+++ src/main/java/com/project/rabs_restaurant_website/RabsRestaurantWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.project.rabs_restaurant_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabsRestaurantWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabsRestaurantWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/rabs_restaurant_website/RabsRestaurantWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rabs_restaurant_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabsRestaurantWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
