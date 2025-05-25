e08284d4c748dfcc1f58900cfd22b3f9d0dfe799
Thendo20 <thendorabali43@gmail.com>
2025-05-25 19:38:43 +0200

    MongoDB setup complete

--- src/test/java/com/project/rabs/restaurant/website/RabsRestaurantWebsiteApplicationTests.java
+++ src/test/java/com/project/rabs/restaurant/website/RabsRestaurantWebsiteApplicationTests.java
@@ -1,13 +1,13 @@
-package com.project.rabs.restaurant.website;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RabsRestaurantWebsiteApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.project.rabs.restaurant.website;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RabsRestaurantWebsiteApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
